aca01d5c1eab9aca4e7374080272ea621a778fd4
Agustin Vargas <agusvargas57@gmail.com>
2023-06-05 23:21:49 -0300

    funcion borrar y resumen actualizados

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const selector = document.querySelector("select");
 const importe = document.querySelector("label[for=monto]");
 const btnResumen = document.querySelector("#resumen");
 const btnBorrar = document.querySelector("#borrar");
+const nombre=document.querySelector('#name');
+const apellido=document.querySelector('#apellido');
+const email=document.querySelector('#email');
 
 const precioTicket= 200;
 //valor del selector ingresado por el usuario
@@ -12,15 +15,25 @@ selector.addEventListener('input',()=>valSelect=selector.value);
 //cantidad de tickets ingresado por el usuario
 let valCant;
 cantTickets.addEventListener('input',()=>valCant=cantTickets.value);
-
+//botones resumen y borrar
 btnResumen.addEventListener('click',totalAPagar);
+btnBorrar.addEventListener('click',borrarForm);
 //funcion que calcula el valor a pagar segun los datos ingresados
-let total;
+
 function totalAPagar(){
 let total;
+
+
     if(valSelect==1)total=(precioTicket*.2)*valCant
     if(valSelect==2)total=(precioTicket*.5)*valCant
     if(valSelect==3)total=(precioTicket*.75)*valCant
 
-importe.insertAdjacentText('beforeend',total);
+importe.textContent='Total a pagar: $ '+total;
+
+}
+
+function borrarForm(){
+    selector.value=0;
+    cantTickets.value=null;
+    importe.textContent="Total a pagar: $";
 }
